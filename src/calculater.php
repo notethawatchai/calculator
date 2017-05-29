e8db9b85b7e77343f62d1e173d8b6db4c946514d
thawatchai krutthin <note57313639@hotmail.com>
2017-05-29 15:42:48 +0700

    Initial commit

---
+++ src/calculater.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace notethawatchai\calculator;
+class Calculator
+{
+    public function add($first_number, $second_number);
+    {
+        return $first_number + $second_number;
+    }
+    public function subtract($first_number, $second_number);
+    {
+        return $first_number - $second_number;
+    }
+    public function multiply($first_number, $second_number);
+    {
+        return $first_number * $second_number;
+    }
+    public function divide($first_number, $second_number);
+    {
+        return $first_number / $second_number;
+    }
+}
